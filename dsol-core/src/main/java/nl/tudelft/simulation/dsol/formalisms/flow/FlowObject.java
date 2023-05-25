d0af0f86cefa5d40187d05151b720089c23cc3fc
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-05-26 01:05:29 +0200

    Issue #17. Updated the abstract FlowObject
    Flow objects now work with Entity rather than with Object.
    Removed the word 'station' everywhere.

--- dsol-core/src/main/java/nl/tudelft/simulation/dsol/formalisms/flow/FlowObject.java
+++ dsol-core/src/main/java/nl/tudelft/simulation/dsol/formalisms/flow/FlowObject.java
@@ -1,16 +1,16 @@
 package nl.tudelft.simulation.dsol.formalisms.flow;
 
-import java.io.Serializable;
-
+import org.djutils.base.Identifiable;
 import org.djutils.event.EventType;
 import org.djutils.event.LocalEventProducer;
+import org.djutils.exceptions.Throw;
 import org.djutils.metadata.MetaData;
 import org.djutils.metadata.ObjectDescriptor;
 
 import nl.tudelft.simulation.dsol.simulators.DevsSimulatorInterface;
 
 /**
- * A station is an object which can receive and/or release other objects that are often called entities.
+ * A FlowObject that can receive and/or release Entity objects.
  * <p>
  * Copyright (c) 2002-2023 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://simulation.tudelft.nl/" target="_blank"> https://simulation.tudelft.nl</a>. The DSOL
@@ -19,10 +19,10 @@ import nl.tudelft.simulation.dsol.simulators.DevsSimulatorInterface;
  * https://https://simulation.tudelft.nl/dsol/docs/latest/license.html</a>.
  * </p>
  * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
- * @param <T> the extended type itself to be able to implement a comparator on the simulation time.
- * @since 1.5
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @param <T> the time type
  */
-public abstract class Station<T extends Number & Comparable<T>> extends LocalEventProducer
+public abstract class FlowObject<T extends Number & Comparable<T>> extends LocalEventProducer implements Identifiable
 {
     /** */
     private static final long serialVersionUID = 20140805L;
@@ -31,84 +31,88 @@ public abstract class Station<T extends Number & Comparable<T>> extends LocalEve
     @SuppressWarnings("checkstyle:visibilitymodifier")
     protected DevsSimulatorInterface<T> simulator;
 
-    /** destination refers to the next station in the process-model chain. */
+    /** destination refers to the next flow object in the process-model chain. */
     @SuppressWarnings("checkstyle:visibilitymodifier")
-    protected Station<T> destination;
+    protected FlowObject<T> destination;
 
-    /** the id of the Station. */
-    private final Serializable id;
+    /** the id of the FlowObject. */
+    private final String id;
 
-    /** RECEIVE_EVENT is fired whenever an entity enters the station. */
-    public static final EventType RECEIVE_EVENT = new EventType(new MetaData("RECEIVE_EVENT", "Object received",
-            new ObjectDescriptor("receivedObject", "received object", Serializable.class)));
+    /** RECEIVE_EVENT is fired whenever an entity enters the flow object. */
+    public static final EventType RECEIVE_EVENT = new EventType(new MetaData("RECEIVE_EVENT", "Entity received",
+            new ObjectDescriptor("receivedObject", "received object", Entity.class)));
 
-    /** RECEIVE_EVENT is fired whenever an entity leaves the station. */
-    public static final EventType RELEASE_EVENT = new EventType(new MetaData("RELEASE_EVENT", "Object released",
-            new ObjectDescriptor("releasedObject", "released object", Serializable.class)));
+    /** RELEASE_EVENT is fired whenever an entity leaves the flow object. */
+    public static final EventType RELEASE_EVENT = new EventType(new MetaData("RELEASE_EVENT", "Entity released",
+            new ObjectDescriptor("releasedObject", "released object", Entity.class)));
 
     /**
-     * constructs a new Station.
-     * @param id Serializable; the id of the Station
+     * Construct a new FlowObject.
+     * @param id String; the id of the FlowObject
      * @param simulator DevsSimulatorInterface&lt;T&gt;; is the simulator on which behavior is scheduled
      */
-    public Station(final Serializable id, final DevsSimulatorInterface<T> simulator)
+    public FlowObject(final String id, final DevsSimulatorInterface<T> simulator)
     {
+        Throw.whenNull(id, "id cannot be null");
+        Throw.whenNull(simulator, "simulator cannot be null");
         this.id = id;
         this.simulator = simulator;
     }
 
     /**
-     * receives an object is invoked whenever an entity arrives.
-     * @param object Object; is the entity
+     * Arrival of a new entity into the flow object.
+     * @param entity Entity; the arriving entity
      */
-    public void receiveObject(final Object object)
+    public void receiveEntity(final Entity<T> entity)
     {
-        this.fireTimedEvent(RECEIVE_EVENT, 1.0, this.simulator.getSimulatorTime());
+        this.fireTimedEvent(RECEIVE_EVENT, entity, this.simulator.getSimulatorTime());
     }
 
     /**
-     * sets the destination of this object.
-     * @param destination Station&lt;T&gt;; defines the next station in the model
+     * Set the destination of this flow object. The destination is the object the entity will go to when leaving this flow
+     * object.
+     * @param destination FlowObject&lt;T&gt;; the next flow object in the model
      */
-    public void setDestination(final Station<T> destination)
+    public void setDestination(final FlowObject<T> destination)
     {
         this.destination = destination;
     }
 
     /**
-     * releases an object.
-     * @param object Object; is the entity
+     * Release an entity, making it flow to the next flow object (the destination) when destination is not null. When destination is
+     * null, the entity will be discarded.
+     * @param entity Entity; the entity to release
      */
-    protected synchronized void releaseObject(final Object object)
+    protected synchronized void releaseObject(final Entity<T> entity)
     {
-        this.fireTimedEvent(RELEASE_EVENT, 0.0, this.simulator.getSimulatorTime());
+        this.fireTimedEvent(RELEASE_EVENT, entity, this.simulator.getSimulatorTime());
         if (this.destination != null)
         {
-            this.destination.receiveObject(object);
+            this.destination.receiveEntity(entity);
         }
     }
 
     /**
-     * Method getDestination.
-     * @return Station is the destination of this station
+     * Return the current destination.
+     * @return FlowObject; the destination of this flow object
      */
-    public Station<T> getDestination()
+    public FlowObject<T> getDestination()
     {
         return this.destination;
     }
 
     /**
-     * @return simulator
+     * Return the simulator.
+     * @return DevsSimultorInterface&lt;T&gt;; the simulator
      */
     public DevsSimulatorInterface<T> getSimulator()
     {
         return this.simulator;
     }
 
-    /**
-     * @return id
-     */
-    public Serializable getId()
+    /** {@inheritDoc} */
+    @Override
+    public String getId()
     {
         return this.id;
     }
@@ -117,7 +121,7 @@ public abstract class Station<T extends Number & Comparable<T>> extends LocalEve
     @Override
     public String toString()
     {
-        return "Station [id=" + this.id + "]";
+        return "FlowObject [id=" + this.id + "]";
     }
 
 }
