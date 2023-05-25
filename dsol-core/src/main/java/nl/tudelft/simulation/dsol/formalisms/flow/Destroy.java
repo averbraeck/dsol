4ba9442c0c8c9ccfe1fea82f1bc17125c8b13f93
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-05-26 01:04:50 +0200

    Issue #17. Updated the Destroy flow object
    Flow objects now work with Entity rather than with Object.
    Statistic for time-in-system added (via Event).

--- dsol-core/src/main/java/nl/tudelft/simulation/dsol/formalisms/flow/Destroy.java
+++ dsol-core/src/main/java/nl/tudelft/simulation/dsol/formalisms/flow/Destroy.java
@@ -1,11 +1,13 @@
 package nl.tudelft.simulation.dsol.formalisms.flow;
 
-import java.io.Serializable;
+import org.djutils.event.EventType;
+import org.djutils.metadata.MetaData;
+import org.djutils.metadata.ObjectDescriptor;
 
 import nl.tudelft.simulation.dsol.simulators.DevsSimulatorInterface;
 
 /**
- * The Destroy station on which entities will be destroyed from the model.
+ * The Destroy flow block where entities will be destroyed from the model.
  * <p>
  * Copyright (c) 2002-2023 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://simulation.tudelft.nl/" target="_blank"> https://simulation.tudelft.nl</a>. The DSOL
@@ -13,31 +15,37 @@ import nl.tudelft.simulation.dsol.simulators.DevsSimulatorInterface;
  * <a href="https://https://simulation.tudelft.nl/dsol/docs/latest/license.html" target="_blank">
  * https://https://simulation.tudelft.nl/dsol/docs/latest/license.html</a>.
  * </p>
- * @author Peter Jacobs, Alexander Verbraeck
- * @param <T> the extended type itself to be able to implement a comparator on the simulation time.
- * @since 1.5
+ * @author Peter Jacobs
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @param <T> the time type itself to be able to implement a comparator on the simulation time.
  */
-public class Destroy<T extends Number & Comparable<T>> extends Station<T>
+public class Destroy<T extends Number & Comparable<T>> extends FlowObject<T>
 {
     /** */
     private static final long serialVersionUID = 1L;
 
+    /** TIME_IN_SYSTEM_EVENT is fired when an entity leaves the system. */
+    public static final EventType TIME_IN_SYSTEM_EVENT = new EventType(new MetaData("TIME_IN_SYSTEM_EVENT", "Time in system",
+            new ObjectDescriptor("Time in system", "time in system", Double.class)));
+
     /**
-     * Constructor for Destroy station.
-     * @param id Serializable; the id of the Station
-     * @param simulator DevsSimulatorInterface&lt;T&gt;; on which is scheduled
+     * Construct a Destroy flow block.
+     * @param id String; the id of the Destroy flow block
+     * @param simulator DevsSimulatorInterface&lt;T&gt;; the simulator
      */
-    public Destroy(final Serializable id, final DevsSimulatorInterface<T> simulator)
+    public Destroy(final String id, final DevsSimulatorInterface<T> simulator)
     {
         super(id, simulator);
     }
 
     /** {@inheritDoc} */
     @Override
-    public synchronized void receiveObject(final Object object)
+    public synchronized void receiveEntity(final Entity<T> entity)
     {
-        this.fireTimedEvent(Station.RECEIVE_EVENT, (Serializable) object, getSimulator().getSimulatorTime());
-        this.fireTimedEvent(Station.RELEASE_EVENT, (Serializable) object, getSimulator().getSimulatorTime());
+        super.receiveEntity(entity);
+        fireTimedEvent(TIME_IN_SYSTEM_EVENT,
+                getSimulator().getSimulatorTime().doubleValue() - entity.getCreationTime().doubleValue(),
+                getSimulator().getSimulatorTime());
     }
 
 }
