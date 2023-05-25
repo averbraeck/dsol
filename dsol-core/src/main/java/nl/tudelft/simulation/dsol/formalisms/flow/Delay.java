6f79794316ff167df945a30812c1410c6a02361d
Alexander Verbraeck <a.verbraeck@gmail.com>
2023-05-26 01:02:43 +0200

    Issue #17. Updated the Delay object

--- dsol-core/src/main/java/nl/tudelft/simulation/dsol/formalisms/flow/Delay.java
+++ dsol-core/src/main/java/nl/tudelft/simulation/dsol/formalisms/flow/Delay.java
@@ -1,13 +1,13 @@
 package nl.tudelft.simulation.dsol.formalisms.flow;
 
-import java.io.Serializable;
+import org.djutils.exceptions.Throw;
 
 import nl.tudelft.simulation.dsol.simtime.dist.DistContinuousSimulationTime;
 import nl.tudelft.simulation.dsol.simulators.DevsSimulatorInterface;
 
 /**
- * The Delay object is a station which delays an entity by some time units. When an entity arrives at a delay object, dsol
- * delays the entity by the resulting time period. During the time delay, the entity is held in the delay object.
+ * The Delay object is a flow object that delays an entity for a given time. After the time delay, the entity is released to the
+ * next flow object. During the time delay, the entity is held in the delay object.
  * <p>
  * Copyright (c) 2002-2023 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://simulation.tudelft.nl/" target="_blank"> https://simulation.tudelft.nl</a>. The DSOL
@@ -16,38 +16,39 @@ import nl.tudelft.simulation.dsol.simulators.DevsSimulatorInterface;
  * https://https://simulation.tudelft.nl/dsol/docs/latest/license.html</a>.
  * </p>
  * @author <a href="https://www.linkedin.com/in/peterhmjacobs">Peter Jacobs </a>
- * @param <T> the extended type itself to be able to implement a comparator on the simulation time.
- * @since 1.5
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * @param <T> the time type
  */
-public class Delay<T extends Number & Comparable<T>> extends Station<T>
+public class Delay<T extends Number & Comparable<T>> extends FlowObject<T>
 {
     /** */
     private static final long serialVersionUID = 20140805L;
 
-    /** delayDistribution which is the distribution defining the delay. */
+    /** the distribution defining the delay. */
     private DistContinuousSimulationTime<T> delayDistribution;
 
     /**
-     * Constructor for Delay.
-     * @param id Serializable; the id of the Station
-     * @param simulator DevsSimulatorInterface&lt;T&gt;; is the simulator
-     * @param delayDistribution DistContinuousSimulationTime&lt;R&gt;; is the delayDistribution
+     * Construct a new Delay flow block.
+     * @param id String; the id of the Delay flow object
+     * @param simulator DevsSimulatorInterface&lt;T&gt;; the simulator
+     * @param delayDistribution DistContinuousSimulationTime&lt;T&gt;; the delay distribution
      */
-    public Delay(final Serializable id, final DevsSimulatorInterface<T> simulator,
+    public Delay(final String id, final DevsSimulatorInterface<T> simulator,
             final DistContinuousSimulationTime<T> delayDistribution)
     {
         super(id, simulator);
+        Throw.whenNull(delayDistribution, "delayDistribution cannot be null");
         this.delayDistribution = delayDistribution;
     }
 
     /** {@inheritDoc} */
     @Override
-    public synchronized void receiveObject(final Object object)
+    public synchronized void receiveEntity(final Entity<T> entity)
     {
-        super.receiveObject(object);
+        super.receiveEntity(entity);
         try
         {
-            this.simulator.scheduleEventRel(this.delayDistribution.draw(), this, "releaseObject", new Object[] {object});
+            this.simulator.scheduleEventRel(this.delayDistribution.draw(), this, "releaseEntity", new Object[] {entity});
         }
         catch (Exception exception)
         {
