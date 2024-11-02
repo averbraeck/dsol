b784c13967dd1d445c616021688e8202be02dd77
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-11-03 00:27:00 +0100

    Issue #36. Make javadoc consistent with Java-17

--- dsol-core/src/main/java/nl/tudelft/simulation/dsol/model/inputparameters/InputParameter.java
+++ dsol-core/src/main/java/nl/tudelft/simulation/dsol/model/inputparameters/InputParameter.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
  * <p>
  * Copyright (c) 2013-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="http://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
- * <p>
+ * </p>
  * @version $Revision$, $LastChangedDate$, by $Author$, initial version Apr 22, 2016 <br>
  * @author <a href="http://www.tbm.tudelft.nl/averbraeck">Alexander Verbraeck</a>
  * @author <a href="http://www.tudelft.nl/pknoppers">Peter Knoppers</a>
--- dsol-core/src/main/java/nl/tudelft/simulation/jstats/ode/integrators/NumericalIntegratorType.java
+++ dsol-core/src/main/java/nl/tudelft/simulation/jstats/ode/integrators/NumericalIntegratorType.java
@@ -11,7 +11,7 @@ import nl.tudelft.simulation.language.DsolRuntimeException;
  * <p>
  * Copyright (c) 2019-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://djunits.org/docs/license.html">DJUNITS License</a>.
- * <p>
+ * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck" target="_blank">Alexander Verbraeck</a>
  */
 public enum NumericalIntegratorType
