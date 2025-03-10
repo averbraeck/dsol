f909b933f4597c73a5c53b971185d6f9f28b7d83
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-03-10 02:02:42 +0100

    Issue #47. Update Delay flow block

--- dsol-flow/src/main/java/nl/tudelft/simulation/dsol/formalisms/flow/Delay.java
+++ dsol-flow/src/main/java/nl/tudelft/simulation/dsol/formalisms/flow/Delay.java
@@ -1,9 +1,16 @@
 package nl.tudelft.simulation.dsol.formalisms.flow;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.djutils.event.EventType;
 import org.djutils.exceptions.Throw;
+import org.djutils.metadata.MetaData;
+import org.djutils.metadata.ObjectDescriptor;
 
 import nl.tudelft.simulation.dsol.simtime.dist.DistContinuousSimulationTime;
 import nl.tudelft.simulation.dsol.simulators.DevsSimulatorInterface;
+import nl.tudelft.simulation.dsol.statistics.SimPersistent;
 
 /**
  * The Delay object is a flow object that delays an entity for a given time. After the time delay, the entity is released to the
@@ -26,32 +33,104 @@ public class Delay<T extends Number & Comparable<T>> extends FlowObject<T>
     /** the distribution defining the delay. */
     private DistContinuousSimulationTime<T> delayDistribution;
 
+    /** list of the current entities in delay. */
+    private final List<Entity<T>> delayedEntityList = new ArrayList<>();
+
+    /** persistent statistic for the number of delayed entities. */
+    private SimPersistent<T> numberDelayedStatistic = null;
+
+    /** NUMBER_DELAYED_EVENT is fired on creation. */
+    public static final EventType NUMBER_DELAYED_EVENT = new EventType(new MetaData("NUMBER_DELAYED_EVENT",
+            "Number of delayed entities)", new ObjectDescriptor("numberDelayed", "number of delayed entities", Integer.class)));
+
     /**
      * Construct a new Delay flow block.
      * @param id String; the id of the Delay flow object
      * @param simulator DevsSimulatorInterface&lt;T&gt;; the simulator
-     * @param delayDistribution DistContinuousSimulationTime&lt;T&gt;; the delay distribution
      */
-    public Delay(final String id, final DevsSimulatorInterface<T> simulator,
-            final DistContinuousSimulationTime<T> delayDistribution)
+    public Delay(final String id, final DevsSimulatorInterface<T> simulator)
     {
         super(id, simulator);
+    }
+
+    /**
+     * Provide the Delay block with a new delay distribution.
+     * @param delayDistribution the new delay distribution
+     * @return the Delay instance for method chaining
+     */
+    public Delay<T> setDelayDistribution(final DistContinuousSimulationTime<T> delayDistribution)
+    {
         Throw.whenNull(delayDistribution, "delayDistribution cannot be null");
         this.delayDistribution = delayDistribution;
+        return this;
+    }
+
+    /**
+     * Turn on the default statistics for this flow block.
+     * @return the Delay instance for method chaining
+     */
+    public Delay<T> setDefaultStatistics()
+    {
+        super.setDefaultFlowObjectStatistics();
+        this.numberDelayedStatistic = new SimPersistent<>(getId() + " number of delayed entities", getSimulator().getModel(),
+                this, Delay.NUMBER_DELAYED_EVENT);
+        this.numberDelayedStatistic.initialize();
+        fireTimedEvent(NUMBER_DELAYED_EVENT, getNumberOfDelayedEntities(), getSimulator().getSimulatorTime());
+        return this;
     }
 
     @Override
     public synchronized void receiveEntity(final Entity<T> entity)
     {
+        Throw.whenNull(this.delayDistribution, "received entity, but delayDistribution is null");
         super.receiveEntity(entity);
-        try
-        {
-            getSimulator().scheduleEventRel(this.delayDistribution.draw(), this, "releaseEntity", new Object[] {entity});
-        }
-        catch (Exception exception)
-        {
-            getSimulator().getLogger().always().warn(exception, "receiveEntity");
-        }
+        this.delayedEntityList.add(entity);
+        fireTimedEvent(NUMBER_DELAYED_EVENT, getNumberOfDelayedEntities(), getSimulator().getSimulatorTime());
+        getSimulator().scheduleEventRel(this.delayDistribution.draw(), this, "releaseEntity", new Object[] {entity});
+    }
+
+    @Override
+    protected synchronized void releaseEntity(final Entity<T> entity)
+    {
+        this.delayedEntityList.remove(entity);
+        fireTimedEvent(NUMBER_DELAYED_EVENT, getNumberOfDelayedEntities(), getSimulator().getSimulatorTime());
+        super.releaseEntity(entity);
+    }
+
+    /**
+     * Return the current delay distribution.
+     * @return the current delay distribution
+     */
+    public DistContinuousSimulationTime<T> getDelayDistribution()
+    {
+        return this.delayDistribution;
+    }
+
+    /**
+     * Return the list of entities that are currently in the Delay block.
+     * @return the list of delayed entities
+     */
+    public List<Entity<T>> getDelayedEntityList()
+    {
+        return this.delayedEntityList;
+    }
+
+    /**
+     * Return the number of entities that are currently in the Delay block.
+     * @return the number of delayed entities
+     */
+    public int getNumberOfDelayedEntities()
+    {
+        return this.delayedEntityList.size();
+    }
+
+    /**
+     * Return whether statistics are turned on for this create block.
+     * @return whether statistics are turned on for this create block.
+     */
+    public boolean hasDefaultStatistics()
+    {
+        return this.numberDelayedStatistic != null;
     }
 
 }
