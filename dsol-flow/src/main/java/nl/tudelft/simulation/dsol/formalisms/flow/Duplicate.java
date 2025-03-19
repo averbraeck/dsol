136ade4ed1582428f1d99d6f087dc6a6a5aab19d
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-03-19 18:14:23 +0100

    Issue #61. Add statistics to Duplicate flow block

--- dsol-flow/src/main/java/nl/tudelft/simulation/dsol/formalisms/flow/Duplicate.java
+++ dsol-flow/src/main/java/nl/tudelft/simulation/dsol/formalisms/flow/Duplicate.java
@@ -2,9 +2,13 @@ package nl.tudelft.simulation.dsol.formalisms.flow;
 
 import java.util.function.Consumer;
 
+import org.djutils.event.EventType;
 import org.djutils.exceptions.Throw;
+import org.djutils.metadata.MetaData;
+import org.djutils.metadata.ObjectDescriptor;
 
 import nl.tudelft.simulation.dsol.simulators.DevsSimulatorInterface;
+import nl.tudelft.simulation.dsol.statistics.SimCounter;
 import nl.tudelft.simulation.jstats.distributions.DistDiscrete;
 import nl.tudelft.simulation.jstats.distributions.DistDiscreteConstant;
 
@@ -28,11 +32,19 @@ public class Duplicate<T extends Number & Comparable<T>> extends FlowObject<T, D
     /** The destination of the duplicates. */
     private FlowObject<T, ?> duplicateDestination;
 
-    /** The distrbution of the number of duplicates to generate. */
+    /** The distribution of the number of duplicates to generate. */
     private DistDiscrete numberCopiesDist = null;
 
     /** the function to apply just before releasing a duplicate entity. */
     private Consumer<Entity<T>> duplicateReleaseFunction = null;
+
+    /** a potential count statistic for the number of released duplicate entities; when null, no statistic is calculated. */
+    private SimCounter<T> countDuplicateReleasedStatistic;
+
+    /** DUPLICATE_RELEASE_EVENT is fired whenever a duplicate entity leaves the flow object. */
+    public static final EventType DUPLICATE_RELEASE_EVENT = new EventType(new MetaData("DUPLICATE_RELEASE_EVENT",
+            "Duplicate entity released",
+            new ObjectDescriptor("releasedDuplicateEntity", "number of released duplicate entities (1)", Integer.class)));
 
     /**
      * Create a new Duplicate flow block.
@@ -96,7 +108,6 @@ public class Duplicate<T extends Number & Comparable<T>> extends FlowObject<T, D
     public synchronized void receiveEntity(final Entity<T> entity)
     {
         Throw.whenNull(this.numberCopiesDist, "distribution for number copies cannot be null when running");
-        Throw.whenNull(this.duplicateDestination, "destination for copies cannot be null when running");
         super.receiveEntity(entity);
         this.releaseEntity(entity);
         long nr = this.numberCopiesDist.draw();
@@ -104,11 +115,61 @@ public class Duplicate<T extends Number & Comparable<T>> extends FlowObject<T, D
         for (int i = 0; i < nr; i++)
         {
             Entity<T> clone = entity.clone();
-            this.fireTimedEvent(FlowObject.RELEASE_EVENT, clone, getSimulator().getSimulatorTime());
+            this.fireTimedEvent(DUPLICATE_RELEASE_EVENT, 1, getSimulator().getSimulatorTime());
             if (this.duplicateReleaseFunction != null)
-                this.duplicateReleaseFunction.accept(entity);
-            this.duplicateDestination.receiveEntity(clone);
+                this.duplicateReleaseFunction.accept(clone);
+            if (this.duplicateDestination != null)
+                this.duplicateDestination.receiveEntity(clone);
         }
+    }
+
+    /**
+     * Turn on the default statistics for this flow block.
+     * @return the Duplicate instance for method chaining
+     */
+    public Duplicate<T> setDefaultStatistics()
+    {
+        super.setDefaultFlowObjectStatistics();
+        this.countDuplicateReleasedStatistic = new SimCounter<>(getId() + " # of released duplicate entities",
+                getSimulator().getModel(), this, DUPLICATE_RELEASE_EVENT);
+        this.countDuplicateReleasedStatistic.initialize();
+        return this;
+    }
+
+    /**
+     * Return whether statistics are turned on for this Duplicate block.
+     * @return whether statistics are turned on for this Duplicate block.
+     */
+    public boolean hasDefaultStatistics()
+    {
+        return this.countDuplicateReleasedStatistic != null;
+    }
+
+    /**
+     * Return the count statistic for the number of released objects
+     * @return the count statistic for the number of released objects, can be null if no statistics are calculated.
+     */
+    public SimCounter<T> getCountDuplicateReleasedStatistic()
+    {
+        return this.countDuplicateReleasedStatistic;
+    }
+
+    /**
+     * Return the destination flow block where the duplicates go.
+     * @return the destination flow block where the duplicates go, can be null if not yet set
+     */
+    public FlowObject<T, ?> getDuplicateDestination()
+    {
+        return this.duplicateDestination;
+    }
+
+    /**
+     * Return the distribution of the number of copies.
+     * @return the distribution of the number of copies, can be null if not yet set
+     */
+    public DistDiscrete getNumberCopiesDist()
+    {
+        return this.numberCopiesDist;
     }
 
 }
