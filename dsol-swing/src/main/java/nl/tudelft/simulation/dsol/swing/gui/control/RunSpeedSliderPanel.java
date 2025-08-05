60974cff3acbe4ab0b6919b5d725f92736371e19
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-08-05 04:13:01 +0200

    Issue #69. Make RealTimeControlPanel use the default RunSpeedSliderPanel

--- dsol-swing/src/main/java/nl/tudelft/simulation/dsol/swing/gui/control/RunSpeedSliderPanel.java
+++ dsol-swing/src/main/java/nl/tudelft/simulation/dsol/swing/gui/control/RunSpeedSliderPanel.java
@@ -1,11 +1,11 @@
 package nl.tudelft.simulation.dsol.swing.gui.control;
 
 import java.awt.Dimension;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Hashtable;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -14,11 +14,14 @@ import javax.swing.SwingConstants;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-import nl.tudelft.simulation.dsol.simulators.DevsRealTimeAnimator;
+import org.djutils.exceptions.Throw;
+
 import nl.tudelft.simulation.dsol.simulators.DevsSimulatorInterface;
+import nl.tudelft.simulation.dsol.simulators.RealTime;
 
 /**
- * JPanel that contains a JSlider for setting the speed of the simulation using a logarithmic scale
+ * JPanel that contains a JSlider for setting the speed of the simulation using a provided scale. Helper methods are available
+ * to create a logarithmic scale.
  * <p>
  * Copyright (c) 2020-2025 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
  * for project information <a href="https://simulation.tudelft.nl/dsol/manual/" target="_blank">DSOL Manual</a>. The DSOL
@@ -33,97 +36,89 @@ public class RunSpeedSliderPanel extends JPanel
     private static final long serialVersionUID = 20150408L;
 
     /** The JSlider that the user sees. */
-    private final JSlider slider;
+    protected JSlider slider;
 
-    /** The ratios used in each decade. */
-    private final int[] ratios;
+    /** the tick values, each representing a speed. */
+    private final List<Double> tickValues = new ArrayList<>();
 
-    /** The values at each tick. */
-    private Map<Integer, Double> tickValues = new LinkedHashMap<>();
+    /** the tick labels to display on the screen. */
+    private final List<String> tickLabels = new ArrayList<>();
 
     /**
-     * Construct a new RunSpeedSliderPanel.
-     * @param minimum the minimum value on the scale (the displayed scale may extend a little further than this value)
-     * @param maximum the maximum value on the scale (the displayed scale may extend a little further than this value)
-     * @param initialValue the initially selected value on the scale
-     * @param ticksPerDecade the number of steps per decade
-     * @param simulator the simulator to change the speed of
+     * Construct a new RunSpeedSliderPanel with tickValues and tickLabels.
+     * @param tickValues the tick values that indicate the speed factors for the simulator
+     * @param tickLabels the tick labels to display on the screen
+     * @param simulator the real-time simulator
+     * @param initialValue the initial value for the speed factor
+     * @param <T> the time type
+     * @param <S> the simulator type
      */
-    public RunSpeedSliderPanel(final double minimum, final double maximum, final double initialValue, final int ticksPerDecade,
-            final DevsSimulatorInterface<?> simulator)
+    public <T extends Number & Comparable<T>, S extends DevsSimulatorInterface<T> & RealTime<T>> RunSpeedSliderPanel(
+            final List<Double> tickValues, final List<String> tickLabels, final S simulator, final double initialValue)
     {
-        if (minimum <= 0 || minimum > initialValue || initialValue > maximum)
+        Throw.whenNull(tickValues, "tickValues");
+        Throw.whenNull(tickLabels, "tickLabels");
+        Throw.whenNull(simulator, "simulator");
+        Throw.when(tickLabels.size() != tickValues.size(), IllegalArgumentException.class,
+                "tickLabels size <> tickValues size");
+        Throw.when(tickLabels.size() == 0, IllegalArgumentException.class, "tickLabels size = 0");
+        for (int i = 1; i < this.tickValues.size(); i++)
         {
-            throw new RuntimeException("Bad (combination of) minimum, maximum and initialValue; "
-                    + "(restrictions: 0 < minimum <= initialValue <= maximum)");
+            if (this.tickValues.get(i - 1) > this.tickValues.get(i))
+                throw new IllegalArgumentException("tick values not in ascending order");
         }
-        switch (ticksPerDecade)
+        this.tickValues.addAll(tickValues);
+        this.tickLabels.addAll(tickLabels);
+        makeSlider(simulator, initialValue);
+    }
+
+    /**
+     * Construct a new RunSpeedSliderPanel with tickValues and tickLabels.
+     * @param tickMap the tick values that indicate the speed factors for the simulator, mapped to the tick labels
+     * @param simulator the real-time simulator
+     * @param initialValue the initial value for the speed factor
+     * @param <T> the time type
+     * @param <S> the simulator type
+     */
+    public <T extends Number & Comparable<T>, S extends DevsSimulatorInterface<T> & RealTime<T>> RunSpeedSliderPanel(
+            final SortedMap<Double, String> tickMap, final S simulator, final double initialValue)
+    {
+        Throw.whenNull(tickMap, "tickMap");
+        Throw.whenNull(simulator, "simulator");
+        Throw.when(tickMap.size() == 0, IllegalArgumentException.class, "tickMap size = 0");
+
+        for (var entry : tickMap.entrySet())
         {
-            case 1:
-                this.ratios = new int[] {1};
-                break;
-            case 2:
-                this.ratios = new int[] {1, 3};
-                break;
-            case 3:
-                this.ratios = new int[] {1, 2, 5};
-                break;
-            default:
-                throw new RuntimeException("Bad ticksPerDecade value (must be 1, 2 or 3)");
+            this.tickValues.add(entry.getKey());
+            this.tickLabels.add(entry.getValue());
         }
-        int minimumTick = (int) Math.floor(Math.log10(minimum / initialValue) * ticksPerDecade);
-        int maximumTick = (int) Math.ceil(Math.log10(maximum / initialValue) * ticksPerDecade);
-        this.slider = new JSlider(SwingConstants.HORIZONTAL, minimumTick, maximumTick + 1, 0);
+
+        makeSlider(simulator, initialValue);
+    }
+
+    /**
+     * Construct the actual slider and change listener. Method can be overridden.
+     * @param simulator the real-time simulator
+     * @param initialValue the initial value for the speed factor
+     * @param <T> the time type
+     * @param <S> the simulator type
+     */
+    public <T extends Number & Comparable<T>,
+            S extends DevsSimulatorInterface<T> & RealTime<T>> void makeSlider(final S simulator, final double initialValue)
+    {
+        this.slider = new JSlider(SwingConstants.HORIZONTAL, 0, this.tickLabels.size() - 1, findTick(initialValue));
         this.slider.setPreferredSize(new Dimension(350, 45));
-        Hashtable<Integer, JLabel> labels = new Hashtable<>();
-        for (int step = 0; step <= maximumTick; step++)
-        {
-            StringBuilder text = new StringBuilder();
-            text.append(this.ratios[step % this.ratios.length]);
-            for (int decade = 0; decade < step / this.ratios.length; decade++)
-            {
-                text.append("0");
-            }
-            this.tickValues.put(step, Double.parseDouble(text.toString()));
-            labels.put(step, new JLabel(text.toString().replace("000", "K")));
-            // System.out.println("Label " + step + " is \"" + text.toString() + "\"");
-        }
-        // Figure out the DecimalSymbol
-        String decimalSeparator =
-                "" + ((DecimalFormat) NumberFormat.getInstance()).getDecimalFormatSymbols().getDecimalSeparator();
-        for (int step = -1; step >= minimumTick; step--)
-        {
-            StringBuilder text = new StringBuilder();
-            text.append("0");
-            text.append(decimalSeparator);
-            for (int decade = (step + 1) / this.ratios.length; decade < 0; decade++)
-            {
-                text.append("0");
-            }
-            int index = step % this.ratios.length;
-            if (index < 0)
-            {
-                index += this.ratios.length;
-            }
-            text.append(this.ratios[index]);
-            labels.put(step, new JLabel(text.toString()));
-            this.tickValues.put(step, Double.parseDouble(text.toString()));
-            // System.out.println("Label " + step + " is \"" + text.toString() + "\"");
-        }
-        labels.put(maximumTick + 1, new JLabel("\u221E"));
-        this.tickValues.put(maximumTick + 1, 1E9);
-        this.slider.setLabelTable(labels);
+        var labelTable = new Hashtable<Integer, JLabel>();
+        for (int i = 0; i < this.tickLabels.size(); i++)
+            labelTable.put(i, new JLabel(this.tickLabels.get(i)));
+        this.slider.setLabelTable(labelTable);
         this.slider.setPaintLabels(true);
         this.slider.setPaintTicks(true);
         this.slider.setMajorTickSpacing(1);
         this.add(this.slider);
 
         // initial value of simulation speed
-        if (simulator instanceof DevsRealTimeAnimator)
-        {
-            DevsRealTimeAnimator<?> clock = (DevsRealTimeAnimator<?>) simulator;
-            clock.setSpeedFactor(RunSpeedSliderPanel.this.tickValues.get(this.slider.getValue()));
-        }
+        simulator.setSpeedFactor(this.tickValues.get(findTick(initialValue)));
 
         // adjust the simulation speed
         this.slider.addChangeListener(new ChangeListener()
@@ -132,91 +127,90 @@ public class RunSpeedSliderPanel extends JPanel
             public void stateChanged(final ChangeEvent ce)
             {
                 JSlider source = (JSlider) ce.getSource();
-                if (!source.getValueIsAdjusting() && simulator instanceof DevsRealTimeAnimator)
+                if (!source.getValueIsAdjusting())
                 {
-                    DevsRealTimeAnimator<?> clock = (DevsRealTimeAnimator<?>) simulator;
-                    clock.setSpeedFactor(((RunSpeedSliderPanel) source.getParent()).getTickValues().get(source.getValue()));
+                    simulator.setSpeedFactor(RunSpeedSliderPanel.this.tickValues.get(source.getValue()));
                 }
             }
         });
     }
 
     /**
-     * Access to tickValues map from within the event handler.
-     * @return Map&lt;Integer, Double&gt; the tickValues map of this TimeWarpPanel
+     * Return the closest tick larger than or equal to the given speed factor.
+     * @param speed the speed factor to search for
+     * @return the closest tick larger than or equal to the given speed factor
      */
-    protected Map<Integer, Double> getTickValues()
+    private int findTick(final double speed)
     {
-        return this.tickValues;
+        for (int i = 0; i < this.tickValues.size(); i++)
+        {
+            if (speed <= this.tickValues.get(i))
+                return i;
+        }
+        return this.tickValues.size() - 1;
     }
 
     /**
-     * Convert a position on the slider to a factor.
-     * @param step the position on the slider
-     * @return the factor that corresponds to step
+     * Helper method to make a logarithmic scale based on a minimum and maximum value, and a number of ticks per decade.
+     * @param minimum the minimum value on the scale (the displayed scale may extend a little further than this value)
+     * @param maximum the maximum value on the scale (the displayed scale may extend a little further than this value)
+     * @param ticksPerDecade the number of steps per decade
+     * @return a sorted map with tick values that indicate the speed factors for the simulator, mapped to tick labels
      */
-    private double stepToFactor(final int step)
+    public static SortedMap<Double, String> makeLogScale(final double minimum, final double maximum, final int ticksPerDecade)
     {
-        int index = step % this.ratios.length;
-        if (index < 0)
-        {
-            index += this.ratios.length;
-        }
-        double result = this.ratios[index];
-        // Make positive to avoid trouble with negative values that round towards 0 on division
-        int power = (step + 1000 * this.ratios.length) / this.ratios.length - 1000; // This is ugly
-        while (power > 0)
-        {
-            result *= 10;
-            power--;
-        }
-        while (power < 0)
-        {
-            result /= 10;
-            power++;
-        }
-        return result;
-    }
+        Throw.when(minimum <= 0 || maximum <= minimum, IllegalArgumentException.class,
+                "Bad minimum or maximum: 0 < minimum < maximum)");
 
-    /**
-     * Retrieve the current TimeWarp factor.
-     * @return the current TimeWarp factor
-     */
-    public double getFactor()
-    {
-        return stepToFactor(this.slider.getValue());
+        int[] ratios = switch (ticksPerDecade)
+        {
+            case 1 -> new int[] {1};
+            case 2 -> new int[] {1, 3};
+            case 3 -> new int[] {1, 2, 5};
+            default -> throw new IllegalArgumentException("Bad ticksPerDecade value (must be 1, 2 or 3)");
+        };
+
+        SortedMap<Double, String> tickMap = new TreeMap<>();
+        int minPower = (int) Math.floor(Math.log10(minimum));
+        int maxPower = (int) Math.ceil(Math.log10(maximum));
+        for (int power = minPower; power <= maxPower; power++)
+        {
+            for (int ratio : ratios)
+            {
+                double speed = Math.pow(10, power) * ratio;
+                String label = String.valueOf(speed);
+                if (power < maxPower || ratio == 1) // only take 10^maxPower * 1
+                {
+                    if (label.endsWith(".0"))
+                        label = label.substring(0, label.length() - 2);
+                    if (label.endsWith("000000"))
+                        label = label.substring(0, label.length() - 6) + "M";
+                    else if (label.endsWith("000"))
+                        label = label.substring(0, label.length() - 3) + "k";
+                    tickMap.put(speed, label);
+                }
+            }
+        }
+        tickMap.put(1.0E12, "\u221E");
+        return tickMap;
     }
 
     @Override
     public String toString()
     {
-        return "TimeWarpPanel [timeWarp=" + this.getFactor() + "]";
+        return "RunSpeedSliderPanel";
     }
 
     /**
-     * Set the time warp factor to the best possible approximation of a given value.
+     * Set the speed factor on the slider to the best possible approximation of a given value.
      * @param factor the requested speed factor
      */
     public void setSpeedFactor(final double factor)
     {
-        int bestStep = -1;
-        double bestError = Double.MAX_VALUE;
-        double logOfFactor = Math.log(factor);
-        for (int step = this.slider.getMinimum(); step <= this.slider.getMaximum(); step++)
+        int tick = findTick(factor);
+        if (this.slider.getValue() != tick)
         {
-            double ratio = getTickValues().get(step); // stepToFactor(step);
-            double logError = Math.abs(logOfFactor - Math.log(ratio));
-            if (logError < bestError)
-            {
-                bestStep = step;
-                bestError = logError;
-            }
-        }
-        // System.out.println("setSpeedfactor: factor is " + factor + ", best slider value is " + bestStep
-        // + " current value is " + this.slider.getValue());
-        if (this.slider.getValue() != bestStep)
-        {
-            this.slider.setValue(bestStep);
+            this.slider.setValue(tick);
         }
     }
 }
